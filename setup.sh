0fa7ae9db78a93ed9d53d7cf910533853088ac49
GGStudy-DDUp <49905579+GGStudy-DDUp@users.noreply.github.com>
2024-02-05 16:04:56 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,17 +2,10 @@
 cd tool
 mkdir dic result tool
 tools=(
-    "blacklanternsecurity/bbot"
-    "screetsec/Sudomy"
     "owasp-amass/amass"
     "laramies/theHarvester"
-    "smicallef/spiderfoot"
-    "tokiakasu/subchase"
     "m3n0sd0n4ld/GooFuzz"
     "projectdiscovery/alterx"
-    "s0md3v/Photon"
-    "Josue87/gotator"
-    "projectdiscovery/subfinder"
     "projectdiscovery/dnsx"
     "opsdisk/metagoofil"
     "shmilylty/OneForAll"
@@ -31,17 +24,11 @@ else
 fi
 cd "$base_pwd/alterx/cmd/alterx" && go build && $sudo_flag cp alterx /usr/bin/
 cd "$base_pwd/amass/cmd/amass" && go build && $sudo_flag cp amass /usr/bin/
-cd "$base_pwd/bbot" && python3 -m pip install pipx && pipx install --pip-args '\--pre' bbot && $sudo_flag ln /root/.local/share/pipx/venvs/bbot/bin/bbot /usr/bin/bbot
 cd "$base_pwd/GooFuzz" && $sudo_flag chmod +x GooFuzz && $sudo_flag cp GooFuzz /usr/bin/
-cd "$base_pwd/Photon" && python3 -m pip install -r requirements.txt
-cd "$base_pwd/spiderfoot" && python3 -m pip install -r requirements.txt
-cd "$base_pwd/subchase/cmd/subchase" && go build && $sudo_flag cp subchase /usr/bin/
-cd "$base_pwd/Sudomy" && python3 -m pip install -r requirements.txt && $sudo_flag cp sudomy /usr/bin/
 cd "$base_pwd/theHarvester" && python3 -m pip install -r requirements.txt
-cd "$base_pwd/gotator" && go build && $sudo_flag cp gotator /usr/bin/
-cd "$base_pwd/subfinder/v2/cmd/subfinder" && go build && $sudo_flag cp subfinder /usr/bin/
 cd "$base_pwd/dnsx/cmd/dnsx" && go build && $sudo_flag cp dnsx /usr/bin/
 $sudo_flag python3 -m pip install fierce
 cd "$base_pwd/metagoofil" && python3 -m pip install -r requirements.txt
 cd "$base_pwd/OneForAll" && python3 -m pip install -r requirements.txt
 cd "$base_pwd/ksubdomain/cmd/ksubdomain" && go build && $sudo_flag cp ksubdomain /usr/bin/
+
